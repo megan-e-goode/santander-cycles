6f58f37cb562c72cf141b4158accb092b7baad1e
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-13 16:11:36 +0100

    created bike test and class.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,1 +1,4 @@
 require 'bike'
+describe Bike do
+  
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,10 @@ describe DockingStation do
   it 'docking station created' do
     expect{DockingStation.new}.not_to raise_error
   end
-
-  it 'can release bike' do
+  before(:each) do
     docking_station = DockingStation.new
+  end
+  it 'can release bike' do
     expect(docking_station).to respond_to(:release_bike)
   end
 end
