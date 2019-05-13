66a1256944d6afeaeeb2b9557740b4a6165746a5
Megan Goode <megan.e.goode@hotmail.com>
2019-05-13 16:39:23 +0100

    Added test bike.working? return true Added method to pass

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,11 @@ describe Bike do
   before(:each) do
     @bike = Bike.new
   end
-  it 'is .working?' do
+  it 'does working? respond_to' do
     expect(@bike).to respond_to(:working?)
+  end
+
+  it 'is working?' do
+    expect(@bike.working?).to eql(true)
   end
 end
