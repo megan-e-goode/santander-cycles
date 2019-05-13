537cba4afe0d4f451d016130e24320a8dbe888c8
Megan Goode <megan.e.goode@hotmail.com>
2019-05-13 15:35:52 +0100

    Added test: Docking station can release bike?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,11 @@
 require 'docking_station'
+
 describe DockingStation do
   it 'docking station created' do
     expect{DockingStation.new}.not_to raise_error
+  end
+
+  it 'can release bike' do
+    expect(DockingStation).to respond_to(:release_bike)
   end
 end
